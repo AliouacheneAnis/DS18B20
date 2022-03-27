9193ed9b3f3bfe5835eeb184fdb286306b10b665
AliouacheneAnis <eaalioua01@monccnb.ca>
2022-03-27 18:27:48 -0300

    Version 1

---
+++ include/Secrets.h
@@ -0,0 +1,29 @@
+/*
+SSID du WIFI ainsi que le mot de passe
+Modifier les paramètre WIFI pour le réseau auquels votre objets doit se brancher
+*/
+// #define WIFI_SECRET_SSID "IDO-OBJECTS"
+// #define WIFI_SECRET_PASS "42Bidules!"
+
+
+#define WIFI_SECRET_SSID "Ste-adele"
+#define WIFI_SECRET_PASS "allo1234"
+
+/*
+Information nécessaire pour le branchement a un Broker MQTT
+Dans le cadre du cours AIIA1013, ne modifié pas cet information
+*/
+
+#define SECRET_MQTT_SERVER_IP "198.164.130.74"
+#define SECRET_MQTT_SERVER_PORT 1883
+
+/*
+Détails sur l'indentification de l'objet
+Cette information provient de l'objet virtuel créer sur le serveur Thingsbord
+*/
+
+#define SECRET_TOKEN  "dlsHTm4rsHKn5QpMcLBW" 
+#define SECRET_DEVICE_ID "a3a17170-aa68-11ec-9235-d318ce92df6e"  
+
+
+
